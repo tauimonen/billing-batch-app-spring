aff0cdb65ac7a488c9d0bd70a5c97265468ba5bf
vgtoui <toni.uimonen@tuni.fi>
2024-02-26 12:22:37 +0200

    Create A Spring Boot project

---
+++ src/main/java/example/billingapp/BillingAppApplication.java
@@ -0,0 +1,13 @@
+package example.billingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/billingapp/BillingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package example.billingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
