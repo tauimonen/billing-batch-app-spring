4b673af4e374c7d7d6003a9245d8165a4de6e2a9
vgtoui <toni.uimonen@tuni.fi>
2024-02-27 13:24:26 +0200

    Implement Job

---
+++ src/main/java/example/billingapp/BillingJob.java
@@ -0,0 +1,16 @@
+package example.billingapp;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+
+public class BillingJob implements Job{
+    @Override
+    public String getName() {
+        return "BillingJob";
+    }
+
+    @Override
+    public void execute(JobExecution execution) {
+        System.out.println("Processing billing job");
+    }
+}
