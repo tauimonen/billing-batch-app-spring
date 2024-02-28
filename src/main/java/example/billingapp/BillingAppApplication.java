5ba7360a263479b1ac17879cb32306607dc8a59f
vgtoui <toni.uimonen@tuni.fi>
2024-02-28 14:04:04 +0200

    Create FilePreparationTasklet that implements Tasklet

--- src/main/java/example/billingapp/BillingAppApplication.java
+++ src/main/java/example/billingapp/BillingAppApplication.java
@@ -2,8 +2,10 @@ package example.billingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({BillingAppConfiguration.class, BatchConfiguration.class})
 public class BillingAppApplication {
 
 	public static void main(String[] args) {
