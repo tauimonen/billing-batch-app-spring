129282c76f7a972afbca59a1b5029d99e772fb27
vgtoui <toni.uimonen@tuni.fi>
2024-02-29 11:18:40 +0200

    Create BillingData record

---
+++ src/main/java/example/billingapp/BillingData.java
@@ -0,0 +1,12 @@
+package example.billingapp;
+
+public record BillingData(
+    int dataYear,
+    int dataMonth,
+    int accountId,
+    String phoneNumber,
+    float dataUsage,
+    int callDuration,
+    int smsCount) {
+}
+
