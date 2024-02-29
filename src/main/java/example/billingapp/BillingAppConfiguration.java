08b78161d98a4066550634a653424d43c0cab1e2
vgtoui <toni.uimonen@tuni.fi>
2024-02-29 15:48:46 +0200

    Configure the file ingestion step

--- src/main/java/example/billingapp/BillingAppConfiguration.java
+++ src/main/java/example/billingapp/BillingAppConfiguration.java
@@ -5,12 +5,18 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.support.JdbcTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BillingAppConfiguration {
@@ -29,6 +35,17 @@ public class BillingAppConfiguration {
     }
 
     @Bean
+    public Step step2(
+            JobRepository jobRepository, JdbcTransactionManager transactionManager,
+            ItemReader<BillingData> billingDataFileReader, ItemWriter<BillingData> billingDataTableWriter) {
+        return new StepBuilder("fileIngestion", jobRepository)
+                .<BillingData, BillingData>chunk(100, transactionManager)
+                .reader(billingDataFileReader)
+                .writer(billingDataTableWriter)
+                .build();
+    }
+
+    @Bean
     public FlatFileItemReader<BillingData> billingDataReader() {
         return new FlatFileItemReaderBuilder<BillingData>()
                 .name("billingDataReader")
@@ -36,6 +53,18 @@ public class BillingAppConfiguration {
                 .delimited()
                 .names("dataYear", "dataMonth", "accountId", "phoneNumber", "dataUsage", "callDuration", "smsCount")
                 .targetType(BillingData.class)
+                .build();
+    }
+
+    @Bean
+    JdbcBatchItemWriter<BillingData> billingDataTableWriter(DataSource dataSource) {
+        String sql =  "insert into billing_data (data_year, data_month, account_id, phone_number, data_usage, call_duration, sms_count) " +
+                "values (:dataYear, :dataMonth, :accountId, :phoneNumber, :dataUsage, :callDuration, :smsCount)";
+
+        return new JdbcBatchItemWriterBuilder<BillingData>()
+                .dataSource(dataSource)
+                .sql(sql)
+                .beanMapped()
                 .build();
     }
 }
